6677766b5172d3cd8a412dd8d9844365df8deb33
tbaddade <thomas@redaxo.org>
2018-07-13 10:52:48 +0200

    Anpassungen von Bimmelbam übernommen

--- src/tasks/svgs.js
+++ src/tasks/svgs.js
@@ -2,6 +2,7 @@ const Task = require('../Task');
 const MultiTask = require('../MultiTask');
 const gulp = require('gulp');
 const colors = require('ansi-colors');
+const log = require('fancy-log');
 const svgstore = require('gulp-svgstore');
 const svgmin = require('gulp-svgmin');
 const size = require('gulp-size');
@@ -20,26 +21,24 @@ class SvgsTask extends Task {
             // prevent pipe breaking caused by errors
             .pipe(plumber())
 
+            // minify
+            .pipe(svgmin())
+
+            // log
+            .pipe(count({
+                message: colors.white('SVG files processed: <%= counter %>'),
+                logger: (message) => log(message)
+            }))
+
             // assemble one SVG
             .pipe(svgstore({
                 inlineSvg: true
-            }))
-
-            .pipe(svgmin({
-                plugins: [{
-                    cleanupIDs: false,
-                    removeUselessDefs: false,
-                    addClassesToSVGElement: {
-                        classNames: ['jabbathehutt'] // a huuuuuuge svg containing it all
-                    }
-                }]
             }))
 
             // stop error prevention
             .pipe(plumber.stop())
 
             .pipe(size({'title': 'SVGs'}))
-            .pipe(count(colors.white('Processed svgs: <%= counter %>')))
 
             .pipe(gulp.dest(this.config.destination));
     }
