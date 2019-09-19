c96bbad3257ee8de41e92ff5bebe1f736bb463e8
Thomas Blum <thomas@redaxo.org>
2019-09-19 17:06:25 +0200

    viewBox erhalten

--- src/tasks/svgs.js
+++ src/tasks/svgs.js
@@ -25,6 +25,8 @@ class SvgsTask extends Task {
             .pipe(svgmin({
                 plugins: [{
                     removeTitle: true
+                }, {
+                    removeViewBox: false
                 }]
             }))
 
